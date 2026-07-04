6deb7ef267bd7f8919bee225ba6afc75206678f3
Nilson Rodrigo <nilsonrodrigues818@gmail.com>
2026-07-04 08:41:17 -0300

    feat: configuracao da estrutura de pastas e react-router-dom

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá, este é o Radar Estágio! </h1>
+      <p>A página inicial está funcionando corretamente.</p>
+    </div>
+  );
+}
